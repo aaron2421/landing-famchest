e1f0010be21be95af7e11fe0012f4722d2348235
aaron_cargame <contacto@cargame.mx>
2022-04-11 11:39:29 -0600

    avance responsive, ya queda bien a la vista, hace falta graficos como el mapa algunos colores, cambiar las imagenes de las empresas por los logos png, faltan las acciones de boton y otras cosas que no se han hablado

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ var phoneBtn = document.getElementById("phone-btn");
 var contactBtn = document.getElementsByClassName("contact-btn")[0];
 var contactBtn2 = document.getElementsByClassName("contact-btn")[1];
 
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
 phoneBtn.onclick = () => {
   if (ancho <= 600) {
     location.href = "tel:+523320350386";
