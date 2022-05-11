95bccc1019d2f0c9ff69f68c5e768a64c8334f27
aaron2421 <aaronariasramos@gmail.com>
2022-05-11 11:25:20 -0600

    favicon, video, comments deleted, action added to buttons

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-var phoneBtn = document.getElementById("phone-btn");
-var contactBtn = document.getElementsByClassName("contact-btn")[0];
-var contactBtn2 = document.getElementsByClassName("contact-btn")[1];
-
-var ancho = window.innerWidth;
-
-function myFunction() {
+function myNavLinks() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
     x.style.display = "none";
@@ -13,18 +7,22 @@ function myFunction() {
   }
 }
 
-phoneBtn.onclick = () => {
-  if (ancho <= 600) {
-    location.href = "tel:+526141903556";
-  } else {
-    location.href = "https://api.whatsapp.com/send?phone=+526141903556";
+window.onload = () => {
+  var phoneBtn = document.getElementById("phone-btn");
+  var contactBtns = document.getElementsByClassName("btn");
+  var ancho = window.innerWidth;
+
+  phoneBtn.onclick = () => {
+    if (ancho <= 600) {
+      location.href = "tel:+526141903556";
+    } else {
+      location.href = "https://api.whatsapp.com/send?phone=+526141903556";
+    }
+  };
+
+  for(var i = 0; i<contactBtns.length; i++) {
+    contactBtns[i].onclick = () => {
+      location.href = "https://calendly.com/arcadia-almacenadoras/30min";
+    }
   }
-};
-
-// contactBtn.onclick = () => {
-//   location.href = "https://calendly.com/arcadia-almacenadoras/30min";
-// }
-
-// contactBtn2.onclick = () => {
-//   location.href = "https://calendly.com/arcadia-almacenadoras/30min";
-// }
+}
